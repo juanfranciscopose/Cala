180afed0cf983a8341d5bc156cdc35c38efac6ad
jfp1989 <juanfranciscopose@gmail.com>
2022-01-12 13:23:17 -0300

    filtros generico: fix en repository

--- back-cala/src/main/java/com/cala/repository/filters/GenderFilterRepository.java
+++ back-cala/src/main/java/com/cala/repository/filters/GenderFilterRepository.java
@@ -1,11 +1,16 @@
 package com.cala.repository.filters;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cala.model.entities.filters.Gender;
-import com.cala.repository.filters.filterable.FilterableRepository;
 
 @Repository
-public interface GenderFilterRepository extends JpaRepository<Gender, Long>, FilterableRepository{
+public interface GenderFilterRepository extends JpaRepository<Gender, Long>{
+	public Optional<Gender> findByName(String name);
+	public Page<Gender> findAll(Pageable pageable);
 }
