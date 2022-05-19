35eb3200e7c76a40dc41e39c6f846ce11a7098bd
jfp1989 <juanfranciscopose@gmail.com>
2022-05-18 23:34:34 -0300

    feat: agrego componente mycheckbox

---
+++ front-cala/src/shared/components/fields/MyCheckbox.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useField, useFormikContext } from 'formik';
+import Typography from '@mui/material/Typography';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+export default function MyCheckbox({
+    name,
+    label,
+    disabled,
+    customChange,
+    checklistValue,
+    ...props
+}) {
+    const [field] = useField(name);
+    const { setFieldValue } = useFormikContext();
+
+    const onChangeValue = event => {
+        if (!disabled) {
+            setFieldValue(name, event.target.checked);
+            customChange && customChange();
+        }
+    }
+
+    return (
+        <FormControlLabel
+            disabled={disabled}
+            label={<Typography variant="caption">{label ? label : ""}</Typography>}
+            control={
+                <Checkbox
+                    {...field}
+                    {...props}
+                    name={name}
+                    checked={Boolean(field.value)} 
+                    defaultChecked={false}
+                    value={checklistValue ? checklistValue : Boolean(field.value)}              
+                    onChange={event => onChangeValue(event)}
+                />}
+        />
+    );
+}
