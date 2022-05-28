8ee5f0018cbf22d570c653e42539266a52993be0
jfp1989 <juanfranciscopose@gmail.com>
2022-05-28 01:06:46 -0300

    feat: test componente ABM table

--- front-cala/src/sections/views/PERS002/ABMPersonas.js
+++ front-cala/src/sections/views/PERS002/ABMPersonas.js
@@ -6,6 +6,34 @@ import FormularioPersona from './FormularioPersona/FormularioPersona';
 
 const TITLE = 'ABMPersonas';
 
+const HEADERS_TABLA = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'age', headerName: 'Age', type: 'number', width: 90 },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const HARDCODE_TABLA_VALUES = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
 const ABMPersonas = () => {
   //modals states
   const [openNew, setOpenNew] = useState(false);
@@ -25,6 +53,11 @@ const ABMPersonas = () => {
 
   const cerrarEdit = () => {
     setOpenEdit(false);
+  }
+
+  const verPersona = (newSelection) => {
+    console.log(newSelection)
+    // abrir modal con itialvalues setiadas en newSelection y inputs grisados 
   }
 
   return (
@@ -50,7 +83,15 @@ const ABMPersonas = () => {
               <Edit/>
             </Fab>
           </Tooltip>
-          <MyABMTable/>
+
+          <MyABMTable 
+            columns={HEADERS_TABLA} 
+            rows={HARDCODE_TABLA_VALUES} 
+            pageSize={25} 
+            height={'max'}
+            customSelectionChange={verPersona}
+          />
+          
           <Tooltip title="Nueva Persona">
             <Fab
              ariant="extended"
@@ -64,7 +105,6 @@ const ABMPersonas = () => {
               <Add/>
             </Fab>
           </Tooltip>
-
       </div>
       <FormularioPersona open={openNew} close={cerrarNew}/>
       <FormularioPersona open={openEdit} close={cerrarEdit} edit={true} initialValues={initPersona}/>
