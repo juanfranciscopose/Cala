392e708a0d9c2cf4363a503dd595441661366b75
jfp1989 <juanfranciscopose@gmail.com>
2022-06-09 00:50:10 -0300

    feat: agrego apiconstant para consultar controller abm personas

---
+++ front-cala/src/constants/apiConstant.js
@@ -0,0 +1,9 @@
+var port = '8080'
+var host = 'localhost'
+var REST_SERVICE_URI = 'http://' + host + ':' + port
+
+module.exports = {
+    apiConstant: {
+        ABMPersonas: `${REST_SERVICE_URI}/people`,
+    },
+}
